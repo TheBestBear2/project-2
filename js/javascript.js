a0b80505490a7fc4daec4564961ec7877a6acdf9
jonathan <157562367+TheBestBear2@users.noreply.github.com>
2024-04-02 13:39:17 -0700

    Add files via upload
    got the API URL

--- js/javascript.js
+++ js/javascript.js
@@ -8,6 +8,10 @@ let generateGif = () => {
     document.querySelector(".wrapper").style.display = "none";
     let q = document.getElementById("search-box").value;
     let gifCount = 10;
+    let finalURL = `https://api.giphy.com/v1/gifs/search?q=${q}&api_key=${apiKey}&limit=${gifCount}
+    &offset=0&rating=g&lang=en&`;
+    document.querySelector(".wrapper").innerHTML = "";
+
 }
 submitBtn.addEventListener("click", generateGif);
 window.addEventListener("load", generateGif);
