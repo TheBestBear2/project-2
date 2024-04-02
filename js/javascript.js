615596e631c7c725dc2bc048f2101fd967206df2
jonathan <157562367+TheBestBear2@users.noreply.github.com>
2024-04-02 13:38:05 -0700

    Add files via upload
    made a gif counter

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,13 @@
 let apiKey = "T7170iEKl4A6f6gwIPrkYEavEc7qkv5X";
 
 let submitBtn = document.getElementById("submit-btn");
+
 let generateGif = () => {
     let loader = document.querySelector(".loader");
     loader.style.display = "block";
     document.querySelector(".wrapper").style.display = "none";
     let q = document.getElementById("search-box").value;
+    let gifCount = 10;
 }
 submitBtn.addEventListener("click", generateGif);
 window.addEventListener("load", generateGif);
