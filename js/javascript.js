b02a8d803f5b7af0544e20ab0708b1c982e26829
jonathan <157562367+TheBestBear2@users.noreply.github.com>
2024-04-02 13:26:39 -0700

    Add files via upload
    displayed loader until gif is load

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,9 @@ let apiKey = "T7170iEKl4A6f6gwIPrkYEavEc7qkv5X";
 
 let submitBtn = document.getElementById("submit-btn");
 let generateGif = () => {
-    
+    let loader = document.querySelector(".loader");
+    loader.style.display = "block";
+    document.querySelector(".wrapper").style.display = "none";
 }
 submitBtn.addEventListener("click", generateGif);
 window.addEventListener("load", generateGif);
