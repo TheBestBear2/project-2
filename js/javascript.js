5330551d8d0655fa25a9c32a2548098c001b1133
jonathan <157562367+TheBestBear2@users.noreply.github.com>
2024-04-02 13:25:31 -0700

    Add files via upload
    put in submitbtn and genbrategif

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,8 @@
 let apiKey = "T7170iEKl4A6f6gwIPrkYEavEc7qkv5X";
 
-
+let submitBtn = document.getElementById("submit-btn");
+let generateGif = () => {
+    
+}
 submitBtn.addEventListener("click", generateGif);
 window.addEventListener("load", generateGif);
